9f9eb510de4213d9d1c7aff965daa9fce33a68ce
Fred Marecesche <fred.marecesche@justice.gov.uk>
2025-11-18 10:39:32 +0000

    Remove example service

--- server/services/index.ts
+++ server/services/index.ts
@@ -1,15 +1,13 @@
 import { dataAccess } from '../data'
 import AuditService from './auditService'
-import ExampleService from './exampleService'
 import CasesService from './casesService'
 
 export const services = () => {
-  const { applicationInfo, hmppsAuditClient, sasClient, casesClient } = dataAccess()
+  const { applicationInfo, hmppsAuditClient, casesClient } = dataAccess()
 
   return {
     applicationInfo,
     auditService: new AuditService(hmppsAuditClient),
-    exampleService: new ExampleService(sasClient),
     casesService: new CasesService(casesClient),
   }
 }

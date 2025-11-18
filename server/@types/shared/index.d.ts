87b9f7f6df0712ed60dc4f1d3f3a53000875e3bc
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2025-11-18 08:30:10 +0000

    Add type generation script

---
+++ server/@types/shared/index.d.ts
@@ -0,0 +1,6 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+
+export type { Case } from './models/Case'
+export type { CasesResponse } from './models/CasesResponse'

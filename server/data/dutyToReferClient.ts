c9e9093afb4f8a8d1357e833c9f83fae90599d50
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2026-03-02 14:56:29 +0000

    Add DutyToReferClient and DutyToReferService

--- server/data/dutyToReferClient.ts
+++ server/data/dutyToReferClient.ts
@@ -4,13 +4,38 @@ import { DutyToReferDto } from '@sas/api'
 import config from '../config'
 import logger from '../../logger'
 import apiPaths from '../paths/api'
+import { SubmitDutyToRefer, UpdateDutyToRefer } from '@sas/ui'
 
 export default class DutyToReferClient extends RestClient {
   constructor(authenticationClient: AuthenticationClient) {
     super('Duty to Refer client', config.apis.sasApi, logger, authenticationClient)
   }
 
+  getAllDutyToRefer(token: string, crn: string) {
+    return this.get<DutyToReferDto[]>({ path: apiPaths.cases.dutyToRefer.index({ crn }) }, asUser(token))
+  }
+
   getDutyToRefer(token: string, crn: string) {
-    return this.get<DutyToReferDto[]>({ path: apiPaths.cases.dutyToRefer({ crn }) }, asUser(token))
+    return this.get<DutyToReferDto>({ path: apiPaths.cases.dutyToRefer.show({ crn }) }, asUser(token))
+  }
+
+  submit(token: string, crn: string, id: string, dutyToRefer: SubmitDutyToRefer) {
+    return this.post<void>(
+      {
+        path: apiPaths.cases.dutyToRefer.submit({ crn, id }),
+        data: dutyToRefer,
+      },
+      asUser(token)
+    )
+  }
+
+  update(token: string, crn: string, id: string, dutyToRefer: UpdateDutyToRefer) {
+    return this.put<void>(
+      {
+        path: apiPaths.cases.dutyToRefer.update({ crn, id }),
+        data: dutyToRefer,
+      },
+      asUser(token)
+    )
   }
 }
--- server/services/dutyToReferService.ts
+++ server/services/dutyToReferService.ts
@@ -1,9 +1,22 @@
+import { SubmitDutyToRefer, UpdateDutyToRefer } from '@sas/ui'
 import { DutyToReferClient } from '../data'
 
 export default class DutyToReferService {
   constructor(private readonly dutyToReferClient: DutyToReferClient) {}
 
+  getAllDutyToRefer(token: string, crn: string) {
+    return this.dutyToReferClient.getAllDutyToRefer(token, crn)
+  }
+
   getDutyToRefer(token: string, crn: string) {
     return this.dutyToReferClient.getDutyToRefer(token, crn)
+  }
+
+  update(token: string, crn: string, id: string, dutyToReferData: UpdateDutyToRefer) {
+    return this.dutyToReferClient.update(token, crn, id, dutyToReferData)
+  }
+
+  submit(token: string, crn: string, id: string, dutyToReferData: SubmitDutyToRefer) {
+    return this.dutyToReferClient.submit(token, crn, id, dutyToReferData)
   }
 }

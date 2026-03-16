e6e84c587098eea36904e6a14a4d4d1832fa1086
Fred Marecesche <fred.marecesche@justice.gov.uk>
2026-03-16 11:13:44 +0000

    Trim all string input

---
+++ server/middleware/trimInput.test.ts
@@ -0,0 +1,74 @@
+import { mock } from 'jest-mock-extended'
+import type { NextFunction, Request, Response } from 'express'
+import trimInput from './trimInput'
+
+describe('trimInput middleware', () => {
+  const token = 'SOME_TOKEN'
+  const dirtyData = {
+    foo: '  spaces and tabs and new lines  \n\n   ',
+    bar: '  ',
+  }
+  const cleanData = {
+    foo: 'spaces and tabs and new lines',
+    bar: '',
+  }
+
+  let request: Request
+  let response: Response
+  let next: NextFunction
+
+  beforeEach(() => {
+    request = mock<Request>({})
+    response = mock<Response>({ locals: { user: { token } } })
+    next = jest.fn()
+
+    jest.resetAllMocks()
+  })
+
+  it('trims the body properties for POST requests', async () => {
+    request.body = dirtyData
+    const middleware = trimInput()
+
+    await middleware(request, response, next)
+
+    expect(request.body).toEqual(cleanData)
+    expect(next).toHaveBeenCalled()
+  })
+
+  it('ignores non-string or nested properties', async () => {
+    const noStrings = {
+      foo: 123,
+      bar: { one: '  ' },
+    }
+    request.body = noStrings
+    const middleware = trimInput()
+
+    await middleware(request, response, next)
+
+    expect(request.body).toEqual(noStrings)
+    expect(next).toHaveBeenCalled()
+  })
+
+  it('trims the query params for GET requests', async () => {
+    request.query = dirtyData
+    const middleware = trimInput()
+
+    await middleware(request, response, next)
+
+    expect(request.query).toEqual(cleanData)
+    expect(next).toHaveBeenCalled()
+  })
+
+  it('ignores requests with no body or query params', async () => {
+    request.body = undefined
+    request.query = {}
+
+    const middleware = trimInput()
+
+    await middleware(request, response, next)
+
+    expect(request.body).toBeUndefined()
+    expect(request.query).toEqual({})
+    expect(next).toHaveBeenCalled()
+  })
+})

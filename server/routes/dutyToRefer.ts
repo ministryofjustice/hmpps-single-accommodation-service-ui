a59544596c9307f0b95b5fe16b6a29389b3e1e6d
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2026-03-18 10:32:10 +0000

    Update form actions for DTR

--- server/routes/dutyToRefer.ts
+++ server/routes/dutyToRefer.ts
@@ -5,8 +5,8 @@ import uiPaths from '../paths/ui'
 export default function dutyToReferRoutes(router: Router, dutyToReferController: DutyToReferController): void {
   router.get(uiPaths.dutyToRefer.guidance.pattern, dutyToReferController.guidance())
   router.get(uiPaths.dutyToRefer.submission.pattern, dutyToReferController.submission())
+  router.post(uiPaths.dutyToRefer.submission.pattern, dutyToReferController.submit())
+  
   router.get(uiPaths.dutyToRefer.outcome.pattern, dutyToReferController.outcome())
-
-  router.post(uiPaths.dutyToRefer.submit.pattern, dutyToReferController.submit())
-  router.post(uiPaths.dutyToRefer.update.pattern, dutyToReferController.update())
+  router.post(uiPaths.dutyToRefer.outcome.pattern, dutyToReferController.update())
 }

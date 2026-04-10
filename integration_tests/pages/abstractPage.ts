a818669d5223f8a24c3ad65f71823a4fa79f814f
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2026-04-10 14:50:32 +0100

    Add no wrap to shouldShowCard

--- integration_tests/pages/abstractPage.ts
+++ integration_tests/pages/abstractPage.ts
@@ -125,7 +125,7 @@ export default class AbstractPage {
       await expect(tag).toBeVisible()
 
       if (cardData.status.colour) {
-        await expect(tag).toHaveClass(`govuk-tag govuk-tag--${cardData.status.colour}`)
+        await expect(tag).toHaveClass(`govuk-tag govuk-tag--${cardData.status.colour} govuk-tag--no-wrap`)
       }
     }
 

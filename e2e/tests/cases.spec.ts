a7aea8baaf708fb75450afc47717e231b40b2153
Fred Marecesche <fred.marecesche@justice.gov.uk>
2026-04-16 14:25:32 +0100

    Use SAS user for E2E tests
    This also makes the test less deterministic, by simply clicking
    on the first available case in the Case list.

--- e2e/tests/cases.spec.ts
+++ e2e/tests/cases.spec.ts
@@ -6,13 +6,10 @@ test('Case list', async ({ page, users: { probation: probationUser } }) => {
   // GIVEN I sign in as a probation user
   await signIn(page, probationUser)
 
-  // WHEN I search for a known CRN
-  await page.getByLabel('Search by name, CRN or prison number').fill('X371199')
-  await page.getByRole('button', { name: 'Apply filters' }).click()
-
   // AND I click on the first result
-  await page.getByRole('link', { name: 'Ben Davies' }).click()
+  const firstCaseName = await page.getByRole('row').nth(1).getByRole('link').textContent()
+  await page.getByRole('link', { name: firstCaseName }).click()
 
   // THEN I should see the case details page
-  await expect(page.getByRole('heading', { name: 'Ben Davies', level: 1 })).toBeVisible()
+  await expect(page.getByRole('heading', { name: firstCaseName, level: 1 })).toBeVisible()
 })

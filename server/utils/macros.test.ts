ef288bbb5437e9532f25adaf74e656ee4cc6460b
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2026-06-08 12:11:31 +0100

    Update statusTag to use classes instead of nowrap

--- server/utils/macros.test.ts
+++ server/utils/macros.test.ts
@@ -21,13 +21,13 @@ describe('Macros', () => {
       expect(statusTag(tag)).toMatchSnapshot()
     })
 
-    it('renders a status tag with noWrap class when noWrap is true', () => {
+    it('renders a status tag with classes when classes is provided', () => {
       const tag: StatusTag = {
         text: 'Qux',
         colour: 'blue',
       }
 
-      expect(statusTag(tag, true)).toMatchSnapshot()
+      expect(statusTag(tag, 'govuk-tag--no-wrap')).toMatchSnapshot()
     })
   })
 

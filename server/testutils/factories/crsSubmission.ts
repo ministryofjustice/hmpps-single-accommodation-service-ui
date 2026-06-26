fd80ed615be0cb83eb080f25de6a3f7d0cc034b8
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2026-06-26 08:57:28 +0100

    Remove draft from crs factory

--- server/testutils/factories/crsSubmission.ts
+++ server/testutils/factories/crsSubmission.ts
@@ -2,13 +2,13 @@ import { Factory } from 'fishery'
 import { CommissionedRehabilitativeServicesDto } from '@sas/api'
 import { faker } from '@faker-js/faker'
 
-const statuses: CommissionedRehabilitativeServicesDto['status'][] = ['LIVE', 'DRAFT', 'COMPLETED', 'WITHDRAWN']
+const statuses: CommissionedRehabilitativeServicesDto['status'][] = ['LIVE', 'COMPLETED', 'WITHDRAWN']
 
 export default Factory.define<CommissionedRehabilitativeServicesDto>(() => {
   const status = faker.helpers.arrayElement(statuses)
 
   return {
     status,
-    submissionDate: status !== 'DRAFT' ? faker.date.past().toISOString().split('T')[0] : null,
+    submissionDate: faker.date.past().toISOString().split('T')[0],
   }
 })

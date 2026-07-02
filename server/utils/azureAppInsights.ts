5784bfdd6e8777d1573511a8eb3fc008c7a78b57
Fred Marecesche <fred.marecesche@justice.gov.uk>
2026-07-02 17:04:01 +0100

    Filter out extra Sentry spans

--- server/utils/azureAppInsights.ts
+++ server/utils/azureAppInsights.ts
@@ -1,7 +1,10 @@
 import { initialiseTelemetry, flushTelemetry, telemetry } from '@ministryofjustice/hmpps-azure-telemetry'
+import { SpanFilterFn, SpanInfo } from '@ministryofjustice/hmpps-azure-telemetry/src/main'
 import applicationInfoSupplier from '../applicationInfo'
 
 const applicationInfo = applicationInfoSupplier()
+
+const filterSentry: SpanFilterFn = (span: SpanInfo) => !span.attributes['sentry.op']
 
 initialiseTelemetry({
   serviceName: applicationInfo.applicationName,
@@ -9,6 +12,7 @@ initialiseTelemetry({
   connectionString: process.env.APPLICATIONINSIGHTS_CONNECTION_STRING,
   debug: process.env.DEBUG_TELEMETRY === 'true',
 })
+  .addFilter(filterSentry)
   .addFilter(telemetry.processors.filterSpanWherePath(['/health', '/ping', '/info', '/assets/*', '/favicon.ico']))
   .addModifier(telemetry.processors.enrichSpanNameWithHttpRoute())
   .startRecording()

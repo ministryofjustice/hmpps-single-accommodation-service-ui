52f3c6077b9355a62c53ec8dff6fe70f95708663
Fred Marecesche <fred.marecesche@justice.gov.uk>
2026-07-22 12:30:11 +0100

    Add step to setup E2E test data

---
+++ test-data-setup/fixtures.ts
@@ -0,0 +1,10 @@
+export const TEST_STAFF = {
+  name: 'TestUser, SAS (PS - PO)',
+  firstName: 'SAS',
+  lastName: 'TestUser',
+}
+
+export const TEST_TEAM = {
+  name: 'Community Accommodation Test Team',
+  provider: 'London',
+}

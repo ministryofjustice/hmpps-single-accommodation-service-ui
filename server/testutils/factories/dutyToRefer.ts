1367ed914ebc0d2474e51f3bb76a939ed053195f
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2026-07-27 10:00:24 +0100

    Undo active being false in dtr factory

--- server/testutils/factories/dutyToRefer.ts
+++ server/testutils/factories/dutyToRefer.ts
@@ -9,19 +9,17 @@ class DutyToReferFactory extends Factory<DutyToReferDto> {
     return this.params({
       status: 'SUBMITTED',
       submission: dtrSubmissionFactory.submitted().build(),
-      active: true,
     })
   }
 
   accepted() {
-    return this.params({ status: 'ACCEPTED', submission: dtrSubmissionFactory.accepted().build(), active: true })
+    return this.params({ status: 'ACCEPTED', submission: dtrSubmissionFactory.accepted().build() })
   }
 
   notAccepted() {
     return this.params({
       status: 'NOT_ACCEPTED',
       submission: dtrSubmissionFactory.notAccepted().build(),
-      active: true,
     })
   }
 

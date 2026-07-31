2975d2a14fa86ba08559e16eb20455a3e57fdbce
Fred Marecesche <fred.marecesche@justice.gov.uk>
2026-07-31 12:01:06 +0100

    Refresh page to ensure case is loaded

--- e2e/tests/cases.spec.ts
+++ e2e/tests/cases.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from '@playwright/test'
 import { test } from '../test'
 import { signIn } from '../steps/signIn'
+import { refreshUntil } from '../utils/refreshUntil'
 
 test('Case list', async ({ page, users: { probation: probationUser }, cases }) => {
   const [forename, surname] = cases.BASE_CASE.name.split(' ')
@@ -8,8 +9,13 @@ test('Case list', async ({ page, users: { probation: probationUser }, cases }) =
   // GIVEN I sign in as a probation user
   await signIn(page, probationUser)
 
-  // AND I click on the first result
-  await page.getByRole('link', { name: `${surname}, ${forename}` }).click()
+  const caseLink = page.getByRole('link', { name: `${surname}, ${forename}` })
+
+  // AND I can see the expected allocated case
+  await refreshUntil(page, () => expect(caseLink).toBeVisible())
+
+  // AND I click on the case link
+  await caseLink.click()
 
   // THEN I should see the case details page
   await expect(page.getByRole('heading', { name: `${forename} ${surname}`, level: 1 })).toBeVisible()

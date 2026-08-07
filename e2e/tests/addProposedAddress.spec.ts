807b74e5828f0d0e6c3348b108a65b162c45b8cd
Fred Marecesche <fred.marecesche@justice.gov.uk>
2026-08-07 16:02:22 +0100

    Update tests to use created case data

--- e2e/tests/addProposedAddress.spec.ts
+++ e2e/tests/addProposedAddress.spec.ts
@@ -10,14 +10,19 @@ import ConfirmAddressPage from '../pages/confirmAddressPage'
 import CheckAnswersPage from '../pages/checkAnswersPage'
 import ConfirmCurrentAddressPage from '../pages/confirmCurrentAddressPage'
 import { ProposedAddressStatus, CurrentAccommodationStatus } from '../data/statuses'
+import { getCaseLink } from '../steps/getCaseLink'
 
-test('Can add and confirm a proposed address', async ({ page, users: { probation: probationUser } }) => {
+test('Can add and confirm a proposed address', async ({
+  page,
+  users: { probation: probationUser },
+  cases: { BASE_CASE },
+}) => {
   // GIVEN I sign in as a probation user
   await signIn(page, probationUser)
 
-  // AND I click on the first result
-  const firstCaseName = await page.getByRole('row').nth(1).getByRole('link').textContent()
-  await page.getByRole('link', { name: firstCaseName }).click()
+  // AND I open the relevant case
+  const { caseLink } = await getCaseLink(page, BASE_CASE)
+  await caseLink.click()
 
   // AND I define the address used throughout the test
   const propertyNameOrNumber = '40'

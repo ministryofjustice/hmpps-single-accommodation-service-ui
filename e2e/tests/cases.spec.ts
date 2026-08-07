0dbc0c6108a664e564687df006a0a3962b87c59e
Fred Marecesche <fred.marecesche@justice.gov.uk>
2026-08-07 11:53:11 +0100

    Add step to get case link based on test case

--- e2e/tests/cases.spec.ts
+++ e2e/tests/cases.spec.ts
@@ -1,30 +1,20 @@
 import { expect } from '@playwright/test'
 // eslint-disable-next-line import/no-extraneous-dependencies
-import {
-  minutesToMilliseconds,
-  secondsToMilliseconds,
-} from '@ministryofjustice/hmpps-probation-integration-e2e-tests/steps/delius/utils/date-time'
+import { minutesToMilliseconds } from '@ministryofjustice/hmpps-probation-integration-e2e-tests/steps/delius/utils/date-time'
 import { test } from '../test'
 import { signIn } from '../steps/signIn'
-import { refreshUntil } from '../utils/refreshUntil'
+import { getCaseLink } from '../steps/getCaseLink'
 
-test('Case list', async ({ page, users: { probation: probationUser }, cases }) => {
+test('Case list', async ({ page, users: { probation: probationUser }, cases: { BASE_CASE } }) => {
   test.setTimeout(minutesToMilliseconds(12))
-
-  const [forename, surname] = cases.BASE_CASE.name.split(' ')
 
   // GIVEN I sign in as a probation user
   await signIn(page, probationUser)
 
-  const caseLink = page.getByRole('link', { name: `${surname}, ${forename}` })
+  // AND I can see the expected case
+  const { caseLink, forename, surname } = await getCaseLink(page, BASE_CASE)
 
-  // AND I can see the expected allocated case
-  await refreshUntil(page, () => expect(caseLink).toBeVisible(), {
-    timeout: minutesToMilliseconds(11),
-    intervals: [secondsToMilliseconds(15)],
-  })
-
-  // AND I click on the case link
+  // WHEN I click on the case link
   await caseLink.click()
 
   // THEN I should see the case details page

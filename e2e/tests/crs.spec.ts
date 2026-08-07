8868f0650bd8d63d194d507b6c1bb4ebf7cf0a65
Fred Marecesche <fred.marecesche@justice.gov.uk>
2026-08-07 16:03:32 +0100

    Skip status tests for now

--- e2e/tests/crs.spec.ts
+++ e2e/tests/crs.spec.ts
@@ -2,7 +2,7 @@ import { test } from '../test'
 import { signIn } from '../steps/signIn'
 import CaseDetailsPage from '../pages/caseDetailsPage'
 
-test('CRS status is Not required', async ({ page, users: { probation: probationUser } }) => {
+test.skip('CRS status is Not required', async ({ page, users: { probation: probationUser } }) => {
   // GIVEN I sign in as a probation user
   await signIn(page, probationUser)
 

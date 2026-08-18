f725f178f0db013f343b06876ccf8033cff54986
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2026-08-18 16:16:41 +0100

    Add accommodationStatusCell tests

--- server/utils/accommodationSummary.test.ts
+++ server/utils/accommodationSummary.test.ts
@@ -1,10 +1,16 @@
 import { AccommodationAddressDetails, AccommodationSummaryDto } from '@sas/api'
-import { accommodationSummariesFactory, accommodationSummaryFactory, addressFactory } from '../testutils/factories'
+import {
+  accommodationSummariesFactory,
+  accommodationSummaryFactory,
+  addressFactory,
+  caseFactory,
+} from '../testutils/factories'
 import {
   accommodationCard,
   accommodationCell,
   accommodationHistoryRows,
   accommodationHistoryTable,
+  accommodationStatusCell,
   accommodationSummaryAddress,
   noFixedAbodeAlert,
 } from './accommodationSummary'
@@ -175,6 +181,52 @@ describe('accommodationSummary', () => {
         nextAccommodation: null,
       })
       expect(noFixedAbodeAlert(accommodationSummaries)).toEqual({ status: 'RISK_OF_NO_FIXED_ABODE', date: null })
+    })
+  })
+
+  describe('accommodationStatusCell', () => {
+    beforeEach(() => {
+      jest.useFakeTimers().setSystemTime(new Date('2025-12-10'))
+    })
+
+    afterEach(() => {
+      jest.useRealTimers()
+    })
+
+    it.each([
+      ['no accommodation summaries', undefined, undefined],
+      ['no case status', accommodationSummariesFactory.build({ caseAccommodationStatus: undefined }), undefined],
+      [
+        'a no fixed abode status',
+        accommodationSummariesFactory.nfa().build(),
+        { status: { text: 'No fixed abode', colour: 'grey' } },
+      ],
+      [
+        'a risk of no fixed abode status',
+        accommodationSummariesFactory.riskOfNfa().build({ caseAccommodationStatusDate: '2025-12-20' }),
+        {
+          status: { text: 'Risk of no fixed abode', colour: 'orange' },
+          dateText: 'From 20 December 2025 (in 10 days)',
+        },
+      ],
+      [
+        'a confirmed settled accommodation',
+        accommodationSummariesFactory.confirmed().build({
+          caseAccommodationStatus: 'SETTLED',
+          caseAccommodationStatusDate: '2025-12-01',
+        }),
+        { status: { text: 'Settled', colour: 'green' }, dateText: 'Since 1 December 2025 (for 9 days)' },
+      ],
+      [
+        'an upcoming confirmed transient accommodation',
+        accommodationSummariesFactory.confirmedUpcoming().build({
+          caseAccommodationStatus: 'TRANSIENT',
+          caseAccommodationStatusDate: '2025-12-20',
+        }),
+        { status: { text: 'Transient', colour: 'pink' }, dateText: 'From 20 December 2025 (in 10 days)' },
+      ],
+    ])('returns the expected status cell for %s', (_, accommodationSummaries, expected) => {
+      expect(accommodationStatusCell(caseFactory.build({ accommodationSummaries }))).toEqual(expected)
     })
   })
 })

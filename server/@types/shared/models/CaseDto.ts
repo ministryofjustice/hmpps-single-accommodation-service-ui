dd270fc72aea989a685c33eddf5c0f8ccee1adb3
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2026-08-19 14:47:18 +0100

    Revert merge conflict issue

--- server/@types/shared/models/CaseDto.ts
+++ server/@types/shared/models/CaseDto.ts
@@ -1,10 +1,9 @@
 /* generated using openapi-typescript-codegen -- do not edit */
 /* istanbul ignore file */
 /* tslint:disable */
-
+/* eslint-disable */
 import type { AccommodationSummariesDto } from './AccommodationSummariesDto'
 import type { AssignedToDto } from './AssignedToDto'
-
 export type CaseDto = {
   forename?: string | null
   middleNames?: string | null

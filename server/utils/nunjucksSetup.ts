838bab6c52d104dfb2d1f584e2c0ea7b427f954e
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2026-09-02 17:23:48 +0100

    Update application name

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -41,7 +41,7 @@ export default function nunjucksSetup(app: express.Express): void {
   app.set('view engine', 'njk')
 
   app.locals.asset_path = '/assets/'
-  app.locals.applicationName = 'Accommodation'
+  app.locals.applicationName = 'Find and manage accommodation'
   app.locals.environmentName = config.environmentName
   app.locals.environmentNameColour = config.environmentName === 'PRE-PRODUCTION' ? 'govuk-tag--green' : ''
   app.locals.applicationInsightsConnectionString = process.env.APPLICATIONINSIGHTS_CONNECTION_STRING || undefined

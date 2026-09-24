uk-gov-mirror/ministryofjustice.hmpps-single-accommodation-service-ui

SAS-1191 Other refferals submission

server/controllers/otherReferralsController.test.ts
@@ -1,0 +1,94 @@
+import { NextFunction, Request, Response } from 'express'
+import { mock } from 'jest-mock-extended'
+import AuditService, { Page } from '../services/auditService'
+import { summaryListRows } from '../utils/dutyToRefer'
+import * as validationUtils from '../utils/validation'
+import CasesService from '../services/casesService'
+import OtherReferralsController from './otherReferralsController'
+import { apiResponseFactory, caseFactory } from '../testutils/factories'
+import OtherReferralsService from '../services/otherReferralsService'
+
+describe('OtherReferralsController', () => {
+  let request: Request
+  const response = mock<Response>({ locals: { user: { username: 'user1', token: 'token-1' } } })
+  const next = mock<NextFunction>()
+
+  const auditService = mock<AuditService>()
+  const casesService = mock<CasesService>()
+  const otherReferralsService = mock<OtherReferralsService>()
+
+  const caseData = caseFactory.build({
+    forename: 'James',
+    surname: 'Smith',
+    crn: 'CRN123',
+  })
+
+  let controller: OtherReferralsController
+  beforeEach(() => {
+    jest.clearAllMocks()
+
+    casesService.getCase.mockResolvedValue(apiResponseFactory.case(caseData))
+
+    request = mock<Request>({
+      id: 'request-id',
+      params: { crn: 'CRN123', id: undefined },
+      body: {},
+      flash: jest.fn(),
+    })
+
+    controller = new OtherReferralsController(auditService, otherReferralsService, casesService)
+    jest
+      .spyOn(validationUtils, 'fetchErrorsAndUserInput')
+      .mockReturnValue({ errors: {}, errorSummary: [], userInput: {} })
+
+    jest.spyOn(validationUtils, 'validateAndFlashErrors')
+    jest.spyOn(validationUtils, 'addGenericErrorToFlash')
+    jest.spyOn(validationUtils, 'addUserInputToFlash')
+  })
+
+  describe('submission', () => {
+    it('renders the submission page for a first referral', async () => {
+      await controller.submission('add')(request, response, next)
+
+      expect(auditService.logPageView).toHaveBeenCalledWith(Page.DUTY_TO_REFER_SUBMISSION, {
+        who: 'user1',
+        correlationId: 'request-id',
+      })
+      expect(casesService.getCase).toHaveBeenCalledWith('token-1', 'CRN123')
+      expect(response.render).toHaveBeenCalledWith('pages/other-referrals/submission', {
+        pageTitle: 'Add other accommodation referral details',
+        backLinkHref: '/cases/CRN123',
+        crn: 'CRN123',
+        tableRows: summaryListRows(caseData),
+        errors: {},
+        errorSummary: [],
+        formValues: {},
+      })
+    })
+
+    it('renders the submission page with errors and user input', async () => {
+      const userInput = {
+        referenceNumber: 'REF123',
+        'submissionDate-year': '2026',
+        'submissionDate-month': '02',
+        'submissionDate-day': '30',
+      }
+      jest.spyOn(validationUtils, 'fetchErrorsAndUserInput').mockReturnValue({
+        errors: { organisationName: { text: 'Enter organisation name' } },
+        errorSummary: [{ text: 'Enter organisation name', href: '#organisationName' }],
+        userInput,
+      })
+
+      await controller.submission('add')(request, response, next)
+
+      expect(response.render).toHaveBeenCalledWith(
+        'pages/other-referrals/submission',
+        expect.objectContaining({
+          errors: { organisationName: { text: 'Enter organisation name' } },
+          errorSummary: [{ text: 'Enter organisation name', href: '#organisationName' }],
+          formValues: userInput,
+        }),
+      )
+    })
+  })
+})

server/controllers/otherReferralsController.ts
@@ -1,0 +1,116 @@
+import { Request, RequestHandler, Response } from 'express'
+import { OtherAccommodationReferralCommand } from '@sas/api'
+import uiPaths from '../paths/ui'
+import { summaryListRows } from '../utils/dutyToRefer'
+import CasesService from '../services/casesService'
+import AuditService, { Page } from '../services/auditService'
+import { addGenericErrorToFlash, fetchErrorsAndUserInput } from '../utils/validation'
+import OtherReferralsService from '../services/otherReferralsService'
+import { dateInputToIsoDate } from '../utils/dates'
+import { validateSubmission, submissionFormValues } from '../utils/otherReferrals'
+
+export type SubmissionFlow = 'add' | 'edit'
+
+export default class OtherReferralsController {
+  constructor(
+    private readonly auditService: AuditService,
+    private readonly otherReferralsService: OtherReferralsService,
+    private readonly casesService: CasesService,
+  ) {}
+
+  submission(flow: SubmissionFlow): RequestHandler {
+    return async (req: Request, res: Response) => {
+      const { token } = res.locals.user
+      const { crn, id } = req.params
+
+      const backLinkHref = id ? uiPaths.dutyToRefer.show({ crn, id }) : uiPaths.cases.show({ crn })
+
+      await this.auditService.logPageView(Page.DUTY_TO_REFER_SUBMISSION, {
+        who: res.locals.user.username,
+        correlationId: req.id,
+      })
+
+      const { tableRows, referral } = await this.getSubmissionPageData(token, crn, id)
+      const { errors, errorSummary, userInput } = fetchErrorsAndUserInput(req)
+
+      const formValues = {
+        ...submissionFormValues(referral),
+        ...userInput,
+      }
+
+      const pageTitleAction = { add: 'Add', addNew: 'Add new', edit: 'Edit' }[flow]
+
+      return res.render('pages/other-referrals/submission', {
+        pageTitle: `${pageTitleAction} other accommodation referral details`,
+        backLinkHref,
+        crn,
+        tableRows,
+        errors,
+        errorSummary,
+        formValues,
+      })
+    }
+  }
+
+  saveSubmission(flow: SubmissionFlow): RequestHandler {
+    return async (req: Request, res: Response) => {
+      const { crn, id } = req.params
+      const { token } = res.locals.user
+      const { organisationName, referenceNumber, website, submissionNote } = req.body
+      const errorRedirect = {
+        add: uiPaths.otherReferrals.submission,
+        edit: uiPaths.otherReferrals.edit,
+      }[flow]({ crn, id })
+
+      if (!validateSubmission(req)) {
+        return res.redirect(errorRedirect)
+      }
+
+      const submissionDate = dateInputToIsoDate(req.body, 'submissionDate')
+
+      try {
+        const submission: OtherAccommodationReferralCommand = {
+          localAuthorityAreaId: 'de653d1f-97fc-4f03-86fd-62c7f6db4515', // TODO: dummy data to make post work
+          status: 'SUBMITTED',
+          submissionDate,
+          referenceNumber,
+          organisationName,
+          website,
+          // email,  // TODO: not in the DTO yet
+          // phone,
+          submissionNote,
+        }
+        // TODO: save an edit
+        // if (id) {
+        //   const { data: referral } = await this.otherReferralsService.getOtherReferralBySubmissionId(token, crn, id)
+        //   if (referral.status !== 'SUBMITTED') {
+        //     submission.status = referral.status
+        //   }
+        //   // await this.otherReferralsService.update(token, crn, id, submission)
+        //   req.flash('success', 'Referral details changed')
+        //  return res.redirect(uiPaths.otherReferrals.show({ crn, id }))
+        // }
+        //
+        await this.otherReferralsService.submit(token, crn, submission)
+
+        req.flash('success', 'Referral details added')
+
+        return res.redirect(uiPaths.cases.show({ crn }))
+      } catch {
+        addGenericErrorToFlash(req, 'There was a problem saving the submission details. Please try again.')
+        return res.redirect(errorRedirect)
+      }
+    }
+  }
+
+  private async getSubmissionPageData(token: string, crn: string, id?: string) {
+    const [{ data: caseData }, { data: referral } = {}] = await Promise.all([
+      this.casesService.getCase(token, crn),
+      id ? this.otherReferralsService.getOtherReferralBySubmissionId(token, crn, id) : undefined,
+    ])
+
+    const tableRows = summaryListRows(caseData)
+
+    return { tableRows, referral }
+  }
+}

server/services/otherReferralsService.test.ts
@@ -1,0 +1,44 @@
+import OtherReferralsService from './otherReferralsService'
+import OtherReferralsClient from '../data/otherReferralsClient'
+import {
+  apiResponseFactory,
+  otherAccommodationReferralCommandFactory,
+  otherAccommodationReferralFactory,
+} from '../testutils/factories'
+import crnFactory from '../testutils/crn'
+
+jest.mock('../data/otherReferralsClient')
+
+describe('OtherReferralsService', () => {
+  const client = new OtherReferralsClient(null) as jest.Mocked<OtherReferralsClient>
+  let service: OtherReferralsService
+
+  const token = 'test-user-token'
+  const crn = crnFactory()
+
+  beforeEach(() => {
+    service = new OtherReferralsService(client)
+  })
+
+  it('should call getOtherReferralBySubmissionId on the api client and return the result', async () => {
+    const otherReferral = otherAccommodationReferralFactory.submitted().build({ crn })
+    const response = apiResponseFactory.otherReferral(otherReferral)
+    client.getOtherReferralBySubmissionId.mockResolvedValue(response)
+
+    const result = await service.getOtherReferralBySubmissionId(token, crn, otherReferral.submission.id)
+
+    expect(client.getOtherReferralBySubmissionId).toHaveBeenCalledWith(token, crn, otherReferral.submission.id)
+    expect(result).toEqual(response)
+  })
+
+  it('should call submit on the api client with dtr command data and return its result', async () => {
+    const command = otherAccommodationReferralCommandFactory.build()
+    const otherReferral = otherAccommodationReferralFactory.submitted().build({ crn })
+    client.submit.mockResolvedValue(otherReferral)
+
+    const result = await service.submit(token, crn, command)
+
+    expect(client.submit).toHaveBeenCalledWith(token, crn, command)
+    expect(result).toEqual(otherReferral)
+  })
+})

ministryofjustice/hmpps-single-accommodation-service-ui

server/controllers/otherReferralsController.test.ts
@@ -1,0 +1,94 @@
+import { NextFunction, Request, Response } from 'express'
+import { mock } from 'jest-mock-extended'
+import AuditService, { Page } from '../services/auditService'
+import { summaryListRows } from '../utils/dutyToRefer'
+import * as validationUtils from '../utils/validation'
+import CasesService from '../services/casesService'
+import OtherReferralsController from './otherReferralsController'
+import { apiResponseFactory, caseFactory } from '../testutils/factories'
+import OtherReferralsService from '../services/otherReferralsService'
+
+describe('OtherReferralsController', () => {
+  let request: Request
+  const response = mock<Response>({ locals: { user: { username: 'user1', token: 'token-1' } } })
+  const next = mock<NextFunction>()
+
+  const auditService = mock<AuditService>()
+  const casesService = mock<CasesService>()
+  const otherReferralsService = mock<OtherReferralsService>()
+
+  const caseData = caseFactory.build({
+    forename: 'James',
+    surname: 'Smith',
+    crn: 'CRN123',
+  })
+
+  let controller: OtherReferralsController
+  beforeEach(() => {
+    jest.clearAllMocks()
+
+    casesService.getCase.mockResolvedValue(apiResponseFactory.case(caseData))
+
+    request = mock<Request>({
+      id: 'request-id',
+      params: { crn: 'CRN123', id: undefined },
+      body: {},
+      flash: jest.fn(),
+    })
+
+    controller = new OtherReferralsController(auditService, otherReferralsService, casesService)
+    jest
+      .spyOn(validationUtils, 'fetchErrorsAndUserInput')
+      .mockReturnValue({ errors: {}, errorSummary: [], userInput: {} })
+
+    jest.spyOn(validationUtils, 'validateAndFlashErrors')
+    jest.spyOn(validationUtils, 'addGenericErrorToFlash')
+    jest.spyOn(validationUtils, 'addUserInputToFlash')
+  })
+
+  describe('submission', () => {
+    it('renders the submission page for a first referral', async () => {
+      await controller.submission('add')(request, response, next)
+
+      expect(auditService.logPageView).toHaveBeenCalledWith(Page.EXTERNAL_REFFERALS_SUBMISSION, {
+        who: 'user1',
+        correlationId: 'request-id',
+      })
+      expect(casesService.getCase).toHaveBeenCalledWith('token-1', 'CRN123')
+      expect(response.render).toHaveBeenCalledWith('pages/other-referrals/submission', {
+        pageTitle: 'Add other accommodation referral details',
+        backLinkHref: '/cases/CRN123',
+        crn: 'CRN123',
+        tableRows: summaryListRows(caseData),
+        errors: {},
+        errorSummary: [],
+        formValues: {},
+      })
+    })
+
+    it('renders the submission page with errors and user input', async () => {
+      const userInput = {
+        referenceNumber: 'REF123',
+        'submissionDate-year': '2026',
+        'submissionDate-month': '02',
+        'submissionDate-day': '30',
+      }
+      jest.spyOn(validationUtils, 'fetchErrorsAndUserInput').mockReturnValue({
+        errors: { organisationName: { text: 'Enter organisation name' } },
+        errorSummary: [{ text: 'Enter organisation name', href: '#organisationName' }],
+        userInput,
+      })
+
+      await controller.submission('add')(request, response, next)
+
+      expect(response.render).toHaveBeenCalledWith(
+        'pages/other-referrals/submission',
+        expect.objectContaining({
+          errors: { organisationName: { text: 'Enter organisation name' } },
+          errorSummary: [{ text: 'Enter organisation name', href: '#organisationName' }],
+          formValues: userInput,
+        }),
+      )
+    })
+  })
+})

server/controllers/otherReferralsController.ts
@@ -1,0 +1,104 @@
+import { Request, RequestHandler, Response } from 'express'
+import { OtherAccommodationReferralCommand } from '@sas/api'
+import uiPaths from '../paths/ui'
+import { summaryListRows } from '../utils/dutyToRefer'
+import CasesService from '../services/casesService'
+import AuditService, { Page } from '../services/auditService'
+import { addGenericErrorToFlash, fetchErrorsAndUserInput } from '../utils/validation'
+import OtherReferralsService from '../services/otherReferralsService'
+import { dateInputToIsoDate } from '../utils/dates'
+import { validateSubmission, submissionFormValues } from '../utils/otherReferrals'
+
+export type SubmissionFlow = 'add' | 'edit'
+
+export default class OtherReferralsController {
+  constructor(
+    private readonly auditService: AuditService,
+    private readonly otherReferralsService: OtherReferralsService,
+    private readonly casesService: CasesService,
+  ) {}
+
+  submission(flow: SubmissionFlow): RequestHandler {
+    return async (req: Request, res: Response) => {
+      const { token } = res.locals.user
+      const { crn, id } = req.params
+
+      const backLinkHref = id ? uiPaths.otherReferrals.show({ crn, id }) : uiPaths.cases.show({ crn })
+
+      await this.auditService.logPageView(Page.EXTERNAL_REFFERALS_SUBMISSION, {
+        who: res.locals.user.username,
+        correlationId: req.id,
+      })
+
+      const { tableRows, referral } = await this.getSubmissionPageData(token, crn, id)
+      const { errors, errorSummary, userInput } = fetchErrorsAndUserInput(req)
+
+      const formValues = {
+        ...submissionFormValues(referral),
+        ...userInput,
+      }
+
+      const pageTitleAction = { add: 'Add', addNew: 'Add new', edit: 'Edit' }[flow]
+
+      return res.render('pages/other-referrals/submission', {
+        pageTitle: `${pageTitleAction} other accommodation referral details`,
+        backLinkHref,
+        crn,
+        tableRows,
+        errors,
+        errorSummary,
+        formValues,
+      })
+    }
+  }
+
+  saveSubmission(flow: SubmissionFlow): RequestHandler {
+    return async (req: Request, res: Response) => {
+      const { crn, id } = req.params
+      const { token } = res.locals.user
+      const { organisationName, referenceNumber, website, submissionNote } = req.body
+      const errorRedirect = {
+        add: uiPaths.otherReferrals.submission,
+        edit: uiPaths.otherReferrals.edit,
+      }[flow]({ crn, id })
+
+      if (!validateSubmission(req)) {
+        return res.redirect(errorRedirect)
+      }
+
+      const submissionDate = dateInputToIsoDate(req.body, 'submissionDate')
+
+      try {
+        const submission: OtherAccommodationReferralCommand = {
+          status: 'SUBMITTED',
+          submissionDate,
+          referenceNumber,
+          organisationName,
+          website,
+          // email,  // TODO: not in the DTO yet
+          // phone,
+          submissionNote,
+        }
+        await this.otherReferralsService.submit(token, crn, submission)
+
+        req.flash('success', 'Referral details added')
+
+        return res.redirect(uiPaths.cases.show({ crn }))
+      } catch {
+        addGenericErrorToFlash(req, 'There was a problem saving the submission details. Please try again.')
+        return res.redirect(errorRedirect)
+      }
+    }
+  }
+
+  private async getSubmissionPageData(token: string, crn: string, id?: string) {
+    const [{ data: caseData }, { data: referral } = {}] = await Promise.all([
+      this.casesService.getCase(token, crn),
+      id ? this.otherReferralsService.getOtherReferralBySubmissionId(token, crn, id) : undefined,
+    ])
+
+    const tableRows = summaryListRows(caseData)
+
+    return { tableRows, referral }
+  }
+}

server/services/otherReferralsService.test.ts
@@ -1,0 +1,44 @@
+import OtherReferralsService from './otherReferralsService'
+import OtherReferralsClient from '../data/otherReferralsClient'
+import {
+  apiResponseFactory,
+  otherAccommodationReferralCommandFactory,
+  otherAccommodationReferralFactory,
+} from '../testutils/factories'
+import crnFactory from '../testutils/crn'
+
+jest.mock('../data/otherReferralsClient')
+
+describe('OtherReferralsService', () => {
+  const client = new OtherReferralsClient(null) as jest.Mocked<OtherReferralsClient>
+  let service: OtherReferralsService
+
+  const token = 'test-user-token'
+  const crn = crnFactory()
+
+  beforeEach(() => {
+    service = new OtherReferralsService(client)
+  })
+
+  it('should call getOtherReferralBySubmissionId on the api client and return the result', async () => {
+    const otherReferral = otherAccommodationReferralFactory.submitted().build({ crn })
+    const response = apiResponseFactory.otherReferral(otherReferral)
+    client.getOtherReferralBySubmissionId.mockResolvedValue(response)
+
+    const result = await service.getOtherReferralBySubmissionId(token, crn, otherReferral.submission.id)
+
+    expect(client.getOtherReferralBySubmissionId).toHaveBeenCalledWith(token, crn, otherReferral.submission.id)
+    expect(result).toEqual(response)
+  })
+
+  it('should call submit on the api client with otherReferral command data and return its result', async () => {
+    const command = otherAccommodationReferralCommandFactory.build()
+    const otherReferral = otherAccommodationReferralFactory.submitted().build({ crn })
+    client.submit.mockResolvedValue(otherReferral)
+
+    const result = await service.submit(token, crn, command)
+
+    expect(client.submit).toHaveBeenCalledWith(token, crn, command)
+    expect(result).toEqual(otherReferral)
+  })
+})